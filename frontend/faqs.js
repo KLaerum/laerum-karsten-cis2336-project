c9929ab0bca3a073119091911f1d99691570fcb2
KLaerum <karsten@laerum.net>
2026-08-18 11:56:42 -0500

    faqs updates

--- frontend/faqs.js
+++ frontend/faqs.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+   document.querySelectorAll('.reveal-text').forEach(paragraph => {
+    const more = paragraph.querySelector('.more-text');
+
+    paragraph.addEventListener('click', () => {
+        more.style.display =
+            more.style.display === 'inline' ? 'none' : 'inline';
+    });
+}); 
 });
